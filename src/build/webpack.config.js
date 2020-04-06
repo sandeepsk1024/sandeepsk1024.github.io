3cdf52517ba6e01613afbaa2f8dd69a30d40d013
Sandeep Khandewale <sandeepsk1024@gmail.com>
2020-04-06 20:51:30 +0530

    pug isProd fix

--- src/build/webpack.config.js
+++ src/build/webpack.config.js
@@ -11,12 +11,12 @@ module.exports = env => {
         module: {
             rules: [
                 { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-                { test: /\.pug$/, exclude: /node_modules/, loader: "pug-loader", options: {globals: {isProd: env.NODE_ENV == "production"}} }
+                { test: /\.pug$/, exclude: /node_modules/, loader: "pug-loader", options: {self: true}}
             ]
         },
         plugins: [
             new webpack.ProgressPlugin(),
-            new HtmlWebpackPlugin({ template: './app/views/index.pug' }),
+            new HtmlWebpackPlugin({ template: './app/views/index.pug', isProd: env.NODE_ENV == "production"}),
             new FaviconsWebpackPlugin({
                 logo: './app/assets/favicon.png',
                 outputPath: './',
