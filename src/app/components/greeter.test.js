1bf05019c2ea3ba8817b505d234a9ab3eb88dbe9
Sandeep Khandewale <sandeepsk1024@gmail.com>
2020-04-11 22:48:02 +0530

    jest

---
+++ src/app/components/greeter.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Greeter from './greeter'
+
+test ("Greeter: it renders without exception: props:none", () => {
+    const component = renderer.create(
+        <Greeter />
+    )
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot()
+})
+
+test ("Greeter: it renders without exception: props:message", () => {
+    const component = renderer.create(
+        <Greeter message={"Hello World!"}></Greeter>
+    )
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot()
+})
--- src/app/components/greeter.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FunctionComponent} from 'react';
-
-type GreeterProps = {
-    message?: string,
-}
-
-const Greeter:FunctionComponent<GreeterProps> = ({message}:GreeterProps) => <h1>{"Greetings for the Day!"}</h1>
-
-export default Greeter 
