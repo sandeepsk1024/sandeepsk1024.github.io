a26b867145258dfa9d58eaa2542986e13377b102
Sandeep Khandewale <sandeepsk1024@gmail.com>
2024-08-30 22:23:38 +0530

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/footer";
 
 function App() {
-
   return (
     <>
-      App
+      <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          <img
+            class="lg:w-1/6 md:w-2/6 w-3/6 mb-10 object-cover object-center rounded"
+            alt="Sandeep Khandewale"
+            src="//media.licdn.com/dms/image/v2/D4D03AQEnLCtGNo6qsw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1662979107389?e=2147483647&v=beta&t=cLqZSK0KqztWYpotcdYIKD1cXz6VBR5d1QTgnvSkZsY"
+          />
+          <div class="text-center lg:w-2/3 w-full">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Sandeep Suresh Khandewale
+            </h1>
+            <p class="mb-8 leading-relaxed">
+              Hello there! Thanks for visiting my github site. I am an
+              experienced Software Engineer. I use ReactJS, TailWindCSS for
+              developing front-ends for websites.
+            </p>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+//https:
---
+++ src/components/footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer class="text-gray-600 body-font">
+      <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
+          © 2024 Sandeep Khandewale —
+          <a
+            href="https://github.com/sandeepsk1024"
+            class="text-gray-600 ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            sandeepsk1024
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
