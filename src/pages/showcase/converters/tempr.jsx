cc7206f279ae61489c173c824214e11ecaa6daf3
Sandeep Khandewale <sandeepsk1024@gmail.com>
2024-09-01 08:45:31 +0530

    tiktoken init

--- src/pages/showcase/converters/tempr.jsx
+++ src/pages/showcase/converters/tempr.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
+import { get_encoding } from "tiktoken";
+const encoding = get_encoding("cl100k_base");
+
+
+
 export default function Temperature() {
+  const [userInput, setUserInput] = useState("");
   return (
     <>
-      Convert temperature
+      <div>
+        <input
+          onChange={(e) => {
+            setUserInput(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div></div>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [wasm(), topLevelAwait(), react()],
+});
