590f2a9f63feab0e7b0009844c27a17e24a866c6
Sandeep Khandewale <sandeepsk1024@gmail.com>
2024-09-02 09:43:47 +0530

    got rid of js-tiktoken. need to study that more

--- src/pages/showcase/converters/tempr.jsx
+++ src/pages/showcase/converters/tempr.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 
-import { get_encoding } from "tiktoken";
-const encoding = get_encoding("cl100k_base");
-
-
 
 export default function Temperature() {
   const [userInput, setUserInput] = useState("");
+
   return (
     <>
       <div>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 import react from '@vitejs/plugin-react-swc'
 
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait(), react()],
+  plugins: [react()],
 });
